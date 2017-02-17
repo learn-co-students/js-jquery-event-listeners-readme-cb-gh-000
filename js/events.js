b94df31e8a9bb6f09c315d6611066f07e772d188
Wen Bo Xie <wenbox323@gmail.com>
2017-02-17 20:47:24 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,32 @@
-//define functions here
+function getIt() {
+  $('p').on('click', function() {
+    alert('Hey!')
+  });
+}
 
-$(document).ready(function(){
+function frameIt() {
+  $('img').on('load', function() {
+    $('img').addClass('tasty')
+  });
+}
 
-// call functions here
+function pressIt() {
+  $('form').on('keydown', function(e) {
+    if (e.which === 71) {
+      alert('g was pressed');
+    }
+  });
+}
 
+function submitIt() {
+  $('form').on('submit', function(e) {
+    alert('Your form is going to be submitted now.')
+  });
+}
+
+$(document).ready(function() {
+  getIt();
+  frameIt();
+  pressIt();
+  submitIt();
 });
