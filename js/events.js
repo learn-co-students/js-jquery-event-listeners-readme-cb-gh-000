ca5e4d4f1974be8d1f099dfb4993da5b8983b837
Rafid Aslam <rafidteam@gmail.com>
2017-02-25 11:30:49 +0700

    Done

--- js/events.js
+++ js/events.js
@@ -1,7 +1,33 @@
-//define functions here
-
 $(document).ready(function(){
-
-// call functions here
-
+    getIt();
+    frameIt();
+    pressIt();
+    submitIt();
 });
+
+function getIt() {
+    $('p').on('click', function () {
+        alert("Hey!");
+    });
+}
+
+function frameIt() {
+    $('img').on('load', function () {
+       $('img').addClass('tasty');
+    });
+}
+
+function pressIt() {
+    $('input').on('keydown', function (key) {
+       if (key.which === 71) {
+
+           alert('You\'ve pressed the g key');
+       }
+    });
+}
+
+function submitIt() {
+    $('form').on('submit', function () {
+        alert("Your form is going to be submitted now.");
+    });
+}
