f3e2b077c9f3b6f53c72704960060d4d9017426b
Julia Nara <julianara888@gmail.com>
2017-03-21 22:24:35 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -5,3 +5,31 @@ $(document).ready(function(){
 // call functions here
 
 });
+
+
+function getIt() {
+  $("p").on('click', function (key) {
+    alert("Hey!");
+
+  });
+}
+
+function frameIt() {
+  $("img").on('load', function() {
+    $("img").addClass('tasty');
+  });
+}
+
+function pressIt() {
+  $("input").on('keydown', function(key) {
+    if (key.which === 71 ) {
+      alert("You have pressed 'g'");
+    }
+  });
+}
+
+function submitIt() {
+  $("form").on("submit", function() {
+      alert("Your form is going to be submitted now.")
+  });
+}
