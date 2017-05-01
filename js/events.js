6bbdc83289e5c7357209f005cdcb7dcc522b7da6
Kaartic Sivaraam <kaarticsivaraam91196@gmail.com>
2017-05-01 12:11:20 +0530

    Done

--- js/events.js
+++ js/events.js
@@ -1,7 +1,30 @@
 //define functions here
+function getIt() {
+  $('p').on('click', () => alert("Hey!"));
+}
+
+function frameIt() {
+  $('img').on('load',function() {
+                        $(this).addClass('tasty')
+                     });
+}
+
+function pressIt() {
+  $('input').on('keydown', (keyEvent) => {
+                              if (keyEvent.key === "g")
+                                alert("You have pressed g");
+                           });
+}
+
+function submitIt() {
+  $('form').on('submit', () => alert("Your form is going to be submitted now."));
+}
 
 $(document).ready(function(){
 
 // call functions here
-
+  getIt();
+  frameIt();
+  pressIt();
+  submitIt();
 });
