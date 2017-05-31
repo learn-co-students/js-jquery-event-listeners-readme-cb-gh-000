2d81b7f3ece6cb03a5eb00167d2c9658f11b7fc6
Edmundo Elizondo <edmundo.geg@hotmail.com>
2017-05-31 21:14:43 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,24 @@
 //define functions here
+function getIt() {
+  $('p').on('click', () => alert('Hey!'))
+}
+function frameIt() {
+  $('img').on('load', (e) => $(e.currentTarget).addClass('tasty'))
+}
+function pressIt() {
+  $('#typing').on('keydown', (e) => {
+    if (e.which === 71)
+      alert("You pressed 'g'!")
+  })
+}
+function submitIt() {
+  $('form').on('submit', (e) => alert('Your form is going to be submitted now.'))
+}
 
 $(document).ready(function(){
-
-// call functions here
-
+  // call functions here
+  getIt()
+  frameIt()
+  pressIt()
+  submitIt()
 });
