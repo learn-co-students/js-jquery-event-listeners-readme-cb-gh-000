8bd0b4a6bb054df7829e7d3cd32210f3e0312669
Robert Wayne <robertjwayne@gmail.com>
2017-06-03 16:57:02 -0400

    Add function submitIt

--- js/events.js
+++ js/events.js
@@ -18,7 +18,15 @@ function pressIt() {
   })
 }
 
+function submitIt() {
+  $( "form" ).on( "submit", function() {
+    alert("Your form is going to be submitted now.")
+  })
+}
+
 $( document ).ready( function() {
   getIt()
   frameIt()
+  pressIt()
+  submitIt()
 });
