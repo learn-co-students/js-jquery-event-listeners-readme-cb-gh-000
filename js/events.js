e72c90acfd3661ceb35a6fa15856d042724a9b5d
Adhi Ariyanto <152008021@student.uksw.edu>
2017-07-26 03:41:39 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,6 +1,31 @@
 //define functions here
+function getIt() {
+  $('p').on('click',function(){
+    alert("Hey!");
+  });
+}
+function frameIt() {
+  $('img').on('load',function() {
+    $( "img" ).addClass('tasty');
+  });
+}
+function submitIt() {
+  $('form').on('submit', function(){
+    alert("Your form is going to be submitted now.");
+  });
+}
+function pressIt() {
+  $('#typing').on('keydown',function(key){
+    if(key.which === 71){
+      alert('g is pressed');
+    }
 
+  });
+}
 $(document).ready(function(){
+    getIt();
+    frameIt();
+    submitIt();
 
 // call functions here
 
