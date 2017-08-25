682030f6a72380693d12390244e5fe999cdb7900
Martyna Chojnacka <martyna.zaa@gmail.com>
2017-08-25 19:18:25 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,34 @@
 //define functions here
+function getIt() {
+  $('p').on('click', function() {
+    alert('Hey!');
+  });
+}
 
+function frameIt() {
+  $('img').on('load', function() {
+    $('img').addClass('tasty').css({ border:'red' });
+  });
+}
+
+function pressIt() {
+  $('input').on('keydown', function(key) {
+    if (key.which === 71) {
+      alert('You pressed G key!');
+    };
+  });
+}
+
+function submitIt() {
+  $('form').on('submit', function(){
+    alert('Your form is going to be submitted now.')
+  })
+}
 $(document).ready(function(){
-
+  getIt();
+  frameIt();
+  pressIt();
+  submitIt();
 // call functions here
 
 });
