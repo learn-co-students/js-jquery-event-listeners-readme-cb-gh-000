b1469cd005893574ae079844de32f4a0dbc21601
Carlos Rivas <carlosr1765@gmail.com>
2017-11-22 00:38:29 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,32 @@
 //define functions here
 
-$(document).ready(function(){
+function getIt() {
+  $('p').on('click', function() {
+    alert('hey!')
+  })
+}
 
-// call functions here
+function frameIt() {
+  $('img').on('load', function() {
+    $('img').addClass('tasty')
+  })
+}
+
+function pressIt() {
+  $('#typing').on('keydown', function(e) {
+    // 'g' corresponds to 71
+    if (e.which === 71) {
+      alert("You pressed 'g'!")
+    }
+  })
+}
+
+function submitIt() {
+  $('form').on('submit', function(e) {
+    alert('Your form is going to be submitted now.')
+  })
+}
+
+$(document).ready(function(){
 
 });
