a8dfc2056e47a20b45c515e56cb36c4067098d4c
Mohamed Benkedadra <hammicristo@gmail.com>
2017-12-24 00:50:13 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,37 @@
 //define functions here
+ function getIt () {
+  $('p').on('click',()=>{
+    alert("Hey!")
+  })
+}
+
+function frameIt() {
+  $('img').on('load',()=>{
+    $('img')[0].classList.add("tasty")
+  })
+}
+
+
+function pressIt() {
+  $('form input').on('keydown',e =>{
+    if (e.which == 71)
+      alert('g is pressed!')
+  })
+}
+
+
+function submitIt() {
+  $('form').on('submit',() =>{
+      alert("Your form is going to be submitted now.")
+  })
+}
+
 
 $(document).ready(function(){
 
 // call functions here
-
+getIt()
+frameIt()
+pressIt()
+submitIt()
 });
