0a482c90cc991c3f77c19c079080e08fff95fd1a
Prabesh Subedi <subediprabesh1@gmail.com>
2018-04-10 14:46:08 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,5 +1,24 @@
 //define functions here
-
+function getIt() {
+  $('p').on('click', (e) => {
+    alert('Hey!');
+  });
+}
+function frameIt() {
+    $('img').on('load', (e) => $('img').addClass('tasty'));
+}
+function pressIt() {
+  $('input#typing').on('keydown', (e) => {
+    if (e.which === 71) {
+      alert("something");
+    }
+  });
+}
+function submitIt() {
+  $('form').on('submit',() => {
+    alert('Your form is going to be submitted now.');
+  });
+}
 $(document).ready(function(){
 
 // call functions here
