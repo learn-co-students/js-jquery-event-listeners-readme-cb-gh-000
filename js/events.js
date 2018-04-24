a54b5374ee30afd17dfe7973c978395feb3b87cd
Abdelrauof <34385173+abdelrauof97@users.noreply.github.com>
2018-04-24 16:31:56 +0200

    Done

--- js/events.js
+++ js/events.js
@@ -1,7 +1,36 @@
-//define functions here
+
+function getIt() {
+  $("p").on("click" ,function() {
+    alert("Hey!");
+  });
+}
+
+function frameIt() {
+  $("img").on("load", function() {
+    image = $("img")[0]
+    image.className = "tasty";
+  });
+}
+
+function pressIt() {
+  $("input").on("keydown", function(key) {
+    if (key.which === 71) {
+      alert("You have pressed the G key!!!")
+    }
+  });
+}
+
+function submitIt() {
+  $(document).on("submit", function() {
+    alert('Your form is going to be submitted now.')
+  });
+}
 
 $(document).ready(function(){
 
-// call functions here
+  getIt();
+  frameIt();
+  pressIt();
+  submitIt();
 
 });
