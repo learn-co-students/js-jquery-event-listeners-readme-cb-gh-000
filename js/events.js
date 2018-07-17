e6f745c1f709c3638de2d34d638ec1929ca90a3d
Engin Bozkurt <engin.bozkurt034@gmail.com>
2018-07-17 00:07:31 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,38 @@
 //define functions here
+//define functions here
+function getIt() {
+  $('p').on('click', function() {
+    alert("Hey!");
+  })
+}
+
+function frameIt() {
+  $('img').on('load', function() {
+    $(this).addClass('tasty');
+  })
+}
+
+function submitIt() {
+  $('form').on('submit', function() {
+    alert("Your form is going to be submitted now.");
+  })
+}
+
+function pressIt() {
+  $('#typing').on('keydown', function(key) {
+    if (key.which == 71) {
+      alert("The g key was pressed.");
+    }
+  })
+}
+
 
 $(document).ready(function(){
 
 // call functions here
+getIt();
+frameIt();
+submitIt();
+pressIt();
 
 });
