4e6b48ca888cae858d7fd5b692c76a9db8b863ff
Brian Uyeda <uyedacb@uw.edu>
2018-08-10 23:06:13 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -3,5 +3,34 @@
 $(document).ready(function(){
 
 // call functions here
-
+getIt()
+frameIt()
+pressIt()
+submitIt()
 });
+
+function getIt() {
+  $('p').on('click', function() {
+    alert("Hey!");
+  });
+}
+
+function frameIt() {
+  $('img').on('load', function() {
+    $('img').attr(class, 'tasty');
+  });
+}
+
+function pressIt() {
+  $('input #typing').on('keydown', function() {
+    if ($("input $typing").val() === "G") {
+      alert("you have pressed the G key");
+    }
+  });
+}
+
+function submitIt() {
+  $('form').on('submit', function() {
+    alert("Your form is going to be submitted now.");
+  });
+}
