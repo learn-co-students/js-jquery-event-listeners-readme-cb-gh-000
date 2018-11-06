6de2aeec000fa574aa6d3726a59b7d800c8577e7
Susanta Biswas <susantasky@gmail.com>
2018-11-06 10:39:55 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -5,3 +5,28 @@ $(document).ready(function(){
 // call functions here
 
 });
+
+function getIt(){
+  $("p").on("click", function(){
+    alert("Hey!");
+  })
+}
+
+function frameIt(){
+  $("img").on("load", function(){
+    $("img").addClass("tasty");
+  })
+}
+
+function pressIt(){
+  $("form input").on("keydown", function(key){
+    if(key.which === 71)
+      alert("pressed G");
+  })
+}
+
+function submitIt(){
+  $("form").on("submit", function(){
+    alert("Your form is going to be submitted now.");
+  });
+}
