40b800452446b83ce4266ee275c588560246bfbd
Yohann Lovell <yoyunyo@hotmail.co.uk>
2019-01-29 13:11:41 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,39 @@
 //define functions here
+function getIt() {
+    $("p").on("click", () => {
+        alert("Hey!");
+        return;
+    });
+};
+
+function frameIt() {
+    $("img").on("load", () => {
+        $("img").addClass("tasty");
+        return;
+    });
+};
+
+function pressIt() {
+    $("input").on("keydown", key => {
+        if (key.which == 71) {
+            alert("You have pressed the G key.")
+        }
+    })
+    return ;
+}
+
+function submitIt() {
+    $("form").on("submit", () => {
+        alert("Your form is going to be submitted now.");
+    });
+    return;
+};
 
 $(document).ready(function(){
 
 // call functions here
-
+    getIt()
+    frameIt()
+    pressIt()
+    submitIt()
 });
