cb1b0258b85af7402149ca096a5b84300a071ded
Alexander Myers <alexander.j.myers@icloud.com>
2019-04-23 00:05:30 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,4 +1,18 @@
-//define functions here
+function getIt(){
+  $('p').on("click", alert("Hey!"))
+}
+
+function frameIt(){
+  $('img').on("load", $('img').addClass('tasty'))
+}
+
+function pressIt(){
+  $('#typing').on('keypress', key => {key.which == 71 ? alert("pressed") : null})
+}
+
+function submitIt(){
+  $('submit').on("click", alert("Your form is going to be submitted now."))
+}
 
 $(document).ready(function(){
 
