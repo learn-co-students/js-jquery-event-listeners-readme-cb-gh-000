53c0e2c25051e7febbb85aec79e3160990b096ba
IVAN V <ivanvalette@hotmail.com>
2019-04-26 04:51:16 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,42 @@
 //define functions here
+function getIt()
+{
+  $('p').on('click', ()=>{
+    alert("Hey!")
+  })
+}
 
+function frameIt()
+{
+  var img = $('img')
+  img.attr('class', 'tasty')
+  $('.tasty').on('load', (e)=>{
+
+  })
+}
+
+function pressIt()
+{
+  $('input#typing').on('keydown', (e)=>{
+
+    if(e.which === 71)
+    {
+      alert('alert')
+    }
+  })
+}
+function submitIt()
+{
+  $('form').on('submit', (e)=>{
+    e.preventDefault();
+    alert("Your form is going to be submitted now.")
+  })
+}
 $(document).ready(function(){
 
 // call functions here
-
+getIt();
+frameIt();
+pressIt();
+submitIt()
 });
