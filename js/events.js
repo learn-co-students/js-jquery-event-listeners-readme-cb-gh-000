dc57be8f2ba23414495a41f31682a0e7f7e793ce
Saurav Singh <snipextt@gmail.com>
2019-12-24 06:09:38 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -3,5 +3,16 @@
 $(document).ready(function(){
 
 // call functions here
-
+getIt()
+frameIt()
 });
+function getIt(){
+  $('p').on('click',()=>{
+    alert('Hey!')
+  });
+}
+function frameIt(){
+  $('img').on('load',()=>{
+    $('img').addClass('tasty')
+  });
+}
