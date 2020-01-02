b482e4e5228a9c4d3dd38d223ef662a537aee9d2
Denny Dang <dennydang@berkeley.edu>
2020-01-02 22:48:33 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,29 @@
 //define functions here
+function getIt() {
+  // function pAlert(k) {
+  //   alert("Hey!");
+  // }
+  $('p').on('click', ()=> alert("Hey!"));
+}
+
+function frameIt() {
+  $('img').on('load', ()=> $('img:first').addClass('tasty'));
+
+}
+
+function pressIt() {
+  $('input').on('keydown', k=> ((k.which==71) ? alert('g') : null ));
+}
+
+function submitIt() {
+  $('form').on('submit', ()=> alert('Your form is going to be submitted now.'));
+}
 
 $(document).ready(function(){
 
 // call functions here
-
+  getIt();
+  frameIt();
+  pressIt();
+  submitIt();
 });
