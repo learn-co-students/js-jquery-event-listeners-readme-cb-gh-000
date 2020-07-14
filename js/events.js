aa97ca69b02119a5ec3bf403411bfa3fbb527857
Abhishek Sisodiya <sisodiya2421@gmail.com>
2020-07-14 18:20:50 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,4 +1,29 @@
 //define functions here
+function getIt() {
+  $('p').on('click', function(){
+    alert("Hey!")
+  });
+}
+
+function frameIt() {
+  $('img').on('load', function(){
+    $('img').attr("class", "tasty")
+  });
+}
+
+function pressIt(){
+  $('form input:first').on('keydown', function(key) {
+      if (key.which == 71) {
+        alert("G pressed");
+      }
+  });
+}
+
+function submitIt() {
+  $('form').on('submit', function() {
+    alert("Your form is going to be submitted now.");
+  });
+}
 
 $(document).ready(function(){
 
